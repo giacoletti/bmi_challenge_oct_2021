d2d770ded75c36ab58c3f226d3dfd34dc5f14c18
giacoletti <giacoletti92@gmail.com>
2021-10-30 23:53:30 +0200

    Adding bootstrap to index.html adding card style to bmi calculator interface

--- cypress/integration/userCanSeeIndexPage.feature.js
+++ cypress/integration/userCanSeeIndexPage.feature.js
@@ -5,7 +5,7 @@ describe('User that navigates to the index page', () => {
     });
 
     it('is expected to see main page with title, input fields and labels', () => {
-        cy.get('h1').should('contain.text', 'BMI Calculator');
+        cy.get('h3').should('contain.text', 'BMI Calculator');
         cy.get('label[id=heightLabel]').should('be.visible').should('contain.text', 'Height (cm):');
         cy.get('input[id=height]').should('be.visible');
         cy.get('label[id=weightLabel]').should('be.visible').should('contain.text', 'Weight (kg):');
