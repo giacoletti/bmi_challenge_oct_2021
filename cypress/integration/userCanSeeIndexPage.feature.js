7b1b762d69d1483a57994b6978b2a83a80c39a0a
giacoletti <giacoletti92@gmail.com>
2021-10-31 13:39:40 +0100

    Adding test to check img visibility and loading adjusting error messages order and minor index.html elements spacing

--- cypress/integration/userCanSeeIndexPage.feature.js
+++ cypress/integration/userCanSeeIndexPage.feature.js
@@ -1,10 +1,12 @@
 describe('User that navigates to the index page', () => {
-    
+
     before(() => {
         cy.visit('/');
     });
 
     it('is expected to see main page with title, input fields and labels', () => {
+        cy.get('img[id=bmiImage]').should('be.visible')
+            .and(($img) => { expect($img[0].naturalWidth).to.be.greaterThan(0) });
         cy.get('h3').should('contain.text', 'BMI Calculator');
         cy.get('label[id=heightLabel]').should('be.visible').should('contain.text', 'Height (cm):');
         cy.get('input[id=height]').should('be.visible');
